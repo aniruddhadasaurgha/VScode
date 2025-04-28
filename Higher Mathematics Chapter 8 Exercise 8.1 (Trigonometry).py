a7769713b3f64f53c041f1aa34eb5948dffadceb
aniruddhadasaurgha <200518@abdurroufcollege.ac.bd>
2025-04-29 00:24:19 +0600

    Second Commit

--- Higher Mathematics Chapter 8 Exercise 8.1 (Trigonometry).py
+++
@@ -1,3 +0,0 @@
-r=float(input('Enter Radian value :'))
-d=r*180/3.1416
-print(d)
--- ICT.py
+++
@@ -1,6 +0,0 @@
-num= int(input("Enter a number :"))
-for i in range(1,11):
-    prod=num*i
-    print(num, '*', i, '=', prod)
-    print()
-    
