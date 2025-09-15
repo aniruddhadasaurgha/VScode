53895d0378e50f31b16d09f5c12823b1f690d10c
aniruddhadasaurgha <200518@abdurroufcollege.ac.bd>
2025-09-15 22:50:27 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+print("I am " , 12 , " years old.")
+name = "Erick"
+print("My name is " + name)
+grade = 9
+print(" I read in grade " + str(grade)  + ".")
+a = float(input("Enter a number : "))
+b = float(input("Enter another number: "))
+c = a + b
+print(c)
+print(type(c))
+
