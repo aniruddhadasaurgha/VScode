e43af72c449b43f7689c648a9c4f7bdb35e385fa
aniruddhadasaurgha <200518@abdurroufcollege.ac.bd>
2025-09-18 23:06:59 +0600

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,35 @@
+name = "Alan"
+age = 24
+weight = 60.5
+is_student = True
+print("My name is ", name , ".")
+print(type(name))
+print("I am ", age, " years old.")
+print(type(age))
+print("I weigh ", weight, " kgs.")
+print(type(weight))
+print("Am I a student? ", is_student)
+print(type(is_student))
+
+age = float(age)
+print("Converted to float: ", age)
+print(type(age))
+weight = int(weight)
+print("Converted to int: ", weight)
+print(type(weight))
+
+number = "1234"
+
+number = int(number)
+print("Converted to int: ", number)
+print(type(number))
+
+number = float(number)
+print("Converted to float: ", number)
+print(type(number))
+
+input_value = int(input("Enter a number :"))
+print("You entered: ", input_value)
+
+
+
---
+++ Operators.py
@@ -0,0 +1,32 @@
+num1 = float(input("Enter first number: "))
+print(type(num1))
+num2 = float(input("Enter second number: "))
+print(type(num2))
+
+Sum = num1 + num2
+print("The sum is: ", Sum)
+print(type(Sum))
+
+Subtraction = num1 - num2
+print("The difference is: ", Subtraction)
+print(type(Subtraction))
+
+Multiplication = num1 * num2
+print("The product is: ", Multiplication)
+print(type(Multiplication))
+
+Division = num1 / num2
+print("The quotient is: ", Division)
+print(type(Division))
+
+Floor_Division = num1 // num2
+print("Floor division: ", Floor_Division)
+print(type(Floor_Division))
+
+Modulus = num1 % num2
+print("The remainder is: ", Modulus)
+print(type(Modulus))
+
+Exponent = num1 ** num2
+print("Exponent: ", Exponent)
+print(type(Exponent))
