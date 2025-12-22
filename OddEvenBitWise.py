fc609dd8648085a3127a7edb5af0adc237e2b7fd
aniruddhadasaurgha <200518@abdurroufcollege.ac.bd>
2025-12-22 22:54:42 +0600

    Add files via upload

---
+++ OddEvenBitWise.py
@@ -0,0 +1,11 @@
+def isEvenOdd( n):
+    if (n ^ 1 == n + 1):
+        return True;
+    else:
+        return False;
+
+number = int(input("Enter a number: "))
+if isEvenOdd(number):
+    print(f"{number} is Even")
+else:
+    print(f"{number} is Odd")
