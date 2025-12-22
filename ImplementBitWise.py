5cc75d306bc8702ce922a8faf49008e71424177c
aniruddhadasaurgha <200518@abdurroufcollege.ac.bd>
2025-12-22 22:43:28 +0600

    Add files via upload

---
+++ ImplementBitWise.py
@@ -0,0 +1,12 @@
+num1 = 10 
+num2 = 4
+
+print("num1 & num2 =", num1 & num2)  
+print("\nnum1 | num2 =", num1 | num2)  
+print("\nnum1 ^ num2 =", num1 ^ num2)  
+print("\n~num1 =", ~num1)                
+
+num1 = 10
+num2 = 4
+print("\nnum1 << 2 =", num1 << 2)      
+print("\nnum2 << 1 =", num2 << 1)      
