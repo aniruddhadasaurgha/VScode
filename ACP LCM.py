3c91c48e548d327bc80e50558b7ef1a1f815cbc8
aniruddhadasaurgha <200518@abdurroufcollege.ac.bd>
2025-12-24 10:42:59 +0600

    Add files via upload

---
+++ ACP LCM.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+def compute_lcm(a, b):
+    if a > b:
+        greater = a
+    else:
+        greater = b
+
+    while True:
+        if greater % a == 0 and greater % b == 0:
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
+lcm = compute_lcm(num1, num2)
+print(f"The LCM of {num1} and {num2} is {lcm}")
